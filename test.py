428c6f1c96b158ad585bd330d400dfe295203832
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2026-01-19 05:26:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import datetime
-a = "{[()]}"
-stack = []
-mapping = {')':'(', ']':'[', '}':'{'}
+duplicate = {}
+listt = [1,2,3,4,5,6,4,1,4,4,3,3,4,6,7,8,9,9,9,8,7,7,7,7,7,8,8,7,7,6,5,4,4,4,6,6,66666]
+for i in listt:
+    duplicate[i] = duplicate.get(i, 0) + 1
 
-for char in a:
-    if char in mapping.values():
-        stack.append(char)
-    elif char in mapping:
-        if not stack or stack.pop() != mapping[char]:
-            print("not ok")
-date = datetime.datetime.now()    
-print(f"This is okay akash jaiswal: {date}")
+print(duplicate)
+
+max_value = max(duplicate.values())
+print(max_value)
+max_key =  [k for k, v in duplicate.items() if v == max_value]
+
+date = datetime.datetime.now()
+
+print(f"{max_key} : {max_value} ==> {date}") 
+ 
+
