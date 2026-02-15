a43100efe927b061e8e142e4e9002ba96606355b
akashjaiswal36 <akash.jaiswal36@gmail.com>
2026-02-16 00:24:53 +0530

    modified

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ listt = [2,3,4,5,6,4,4,4,3,3,4,6,7,8,9,9,9,8,7,7,7,7,7,8,8,7,7,6,5,4,4,4,6,6,666
 for i in listt:
     duplicate[i] = duplicate.get(i, 0) + 1
 
-print(duplicate)
 
 max_value = max(duplicate.values())
 print(max_value)
