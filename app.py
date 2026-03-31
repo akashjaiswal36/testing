141e48d34c4f3b3055a9a0b498f78f3bbceb4b27
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2026-03-31 10:13:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello():
         tip="Built with Flask, shipped by Jenkins, running in Docker.",
         UI="THis is from feature/ui branch",
         NOTE="This is new   pr",
-        BRANCH="feaute branch is deleted WTF WTF WTF WTF WTF......."
+        BRANCH="feaute branch is deleted , Will create new branch"
     )
 
 if __name__ == "__main__":
